c0321238f3fec6fce369bbcc67c7de8518ac9e2c
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2022-11-19 11:40:21 -0600

    docs: add introduction for raj traders application

---
+++ src/main/java/com/unlockprogramming/rta/RajTradersApplication.java
@@ -0,0 +1,13 @@
+package com.unlockprogramming.rta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajTradersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajTradersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unlockprogramming/rta/RajTradersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unlockprogramming.rta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RajTradersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
