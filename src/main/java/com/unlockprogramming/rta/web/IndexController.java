6afa919ebb947b37bd26a851f9d375d1eb21e303
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2022-11-21 10:39:58 -0600

    docs: add introduction for raj traders application

--- src/main/java/com/unlockprogramming/rta/web/IndexController.java
+++ src/main/java/com/unlockprogramming/rta/web/IndexController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+    @GetMapping("/")
+    public String index(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("yourName", name);
+        return "index";
     }
 
 }
